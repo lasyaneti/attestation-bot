0b4427d6b08802472c38be1efd6335751a3dbc89
lasyaneti <neti.lasya@gmail.com>
2021-05-11 11:06:50 -0700

    nextpage function defined

--- AttestationBot.py
+++ AttestationBot.py
@@ -2,88 +2,70 @@ from selenium import webdriver
 from selenium.webdriver.support.select import Select
 import time
 
-form = webdriver.Chrome()
-form.get("https://issaquah.az1.qualtrics.com/jfe/form/SV_6nepDReMDTALzMx")
+def nextpage(): 
+    nextButton = driver.find_element_by_id("NextButton") #define var on every page to avoid slate element exception
+    nextButton.click()
+    time.sleep(2) #time to load elements of new page
 
-time.sleep(2) #repeated after every page to avoid "no such element error"
+driver = webdriver.Chrome()
+driver.get("https://issaquah.az1.qualtrics.com/jfe/form/SV_6nepDReMDTALzMx")
 
-nextButton: form.find_element_by_id("NextButton") #repetition due to stale element error. needs fixing
-nextButton.click() 
+time.sleep(2)
+nextpage()
 
-time.sleep(2) 
 
-email = form.find_element_by_id("QR~QID22~1")
-myEmail = "XXX@gmail.com"
+email = driver.find_element_by_id("QR~QID22~1")
+myEmail = "XXX"
 email.send_keys(myEmail)
-nextButton = form.find_element_by_id("NextButton")
-nextButton.click()
 
-time.sleep(2) 
+nextpage()
 
-firstname = form.find_element_by_id("QR~QID24~1")
+firstname = driver.find_element_by_id("QR~QID24~1")
 myFirstName = "Lasya"
 firstname.send_keys(myFirstName)
-lastname = form.find_element_by_id("QR~QID24~2")
+lastname = driver.find_element_by_id("QR~QID24~2")
 myLastName = "Neti"
 lastname.send_keys(myLastName)
-nextButton = form.find_element_by_id("NextButton")
-nextButton.click()
 
-time.sleep(2) 
+nextpage()
+time.sleep(2)
 
-form.find_element_by_id("QID29-2-label").click() #select visitor type as current student
-dropdown = form.find_element_by_id("QR~QID33")
-myGrade = form.find_element_by_id("QR~QID33~13")
+driver.find_element_by_id("QID29-2-label").click() #select visitor type as current student
+dropdown = driver.find_element_by_id("QR~QID33")
+myGrade = driver.find_element_by_id("QR~QID33~13")
 time.sleep(2) #time to load dropdown element
 Select(dropdown).select_by_visible_text("Grade 12")
-nextButton = form.find_element_by_id("NextButton")
-nextButton.click()
 
-time.sleep(2)
+nextpage()
 
-nextButton = form.find_element_by_id("NextButton")
-nextButton.click()
+nextpage()
 
-time.sleep(2)
+driver.find_element_by_id("QID21-1-label").click() #entering facility
 
-form.find_element_by_id("QID21-1-label").click() #entering facility
-nextButton = form.find_element_by_id("NextButton")
-nextButton.click()
+nextpage()
 
-time.sleep(2)
+driver.find_element_by_id("QID69-21-label").click() #specify facility
 
-form.find_element_by_id("QID69-21-label").click() #specify facility
-nextButton = form.find_element_by_id("NextButton")
-nextButton.click()
+nextpage()
 
-time.sleep(2)
+driver.find_element_by_id("QID11-1-label").click() #consent
 
-form.find_element_by_id("QID11-1-label").click() #consent
-nextButton = form.find_element_by_id("NextButton")
-nextButton.click()
+nextpage()
 
-time.sleep(2)
+driver.find_element_by_id("QID34-19-label").click() #symptoms
 
-form.find_element_by_id("QID34-19-label").click() #symptoms
-nextButton = form.find_element_by_id("NextButton")
-nextButton.click()
+nextpage()
 
-time.sleep(2)
+driver.find_element_by_id("QID63-6-label").click() #distancing from affected people
 
-form.find_element_by_id("QID63-6-label").click() #distancing from affected people
-nextButton = form.find_element_by_id("NextButton")
-nextButton.click()
+nextpage()
 
-time.sleep(2)
+driver.find_element_by_id("QID64-6-label").click() #COVID test result
 
-form.find_element_by_id("QID64-6-label").click() #COVID test result
-nextButton = form.find_element_by_id("NextButton")
-nextButton.click()
+nextpage()
 
-time.sleep(2)
+driver.find_element_by_id("QID65-6-label").click() #quarantine 
 
-form.find_element_by_id("QID65-6-label").click() #quarantine 
-nextButton = form.find_element_by_id("NextButton")
-nextButton.click()
+nextpage()
 
-form.close()
+driver.close()
